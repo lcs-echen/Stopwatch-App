be041828b8ee0c4a724e044f7c6f6bdd4fe1c4df
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2022-11-09 09:13:09 -0500

    Initial Commit

---
+++ Stopwatch App/Stopwatch_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stopwatch_AppApp.swift
+//  Stopwatch App
+//
+//  Created by Evelyn Chen on 2022-11-09.
+//
+
+import SwiftUI
+
+@main
+struct Stopwatch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
